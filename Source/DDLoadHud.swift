2c20e44a8f2e3339962541493a35a31b2dcf7fc9
davis <cuirhong@126.com>
2019-04-12 15:44:06 +0800

    add stop load

--- Source/DDLoadHud.swift
+++ Source/DDLoadHud.swift
@@ -25,7 +25,8 @@ open class DDLoadHud:UIView  {
     // MARK:加载的颜色
     open lazy var loadColor:UIColor = UIColor.white
     
-    // MARK:y
+    // MARK:旋转的速度,默认是1
+    open lazy var animationSpeed:TimeInterval = 1
     
    
     public override init(frame: CGRect) {
@@ -72,11 +73,18 @@ open class DDLoadHud:UIView  {
         rotationAnim.fromValue = 0 // 开始角度
         rotationAnim.toValue = Double.pi * 2 //结束角度
         rotationAnim.repeatCount = MAXFLOAT // 重复次数
-        rotationAnim.duration = 1 // 一圈所需要的时间
+        rotationAnim.duration = 1/animationSpeed // 一圈所需要的时间
         rotationAnim.isRemovedOnCompletion = false //默认是true，切换到其他控制器再回来，动画效果会消失，需要设置成false，动画就不会停了
         progressDrawingview.layer.add(rotationAnim, forKey: nil) // 给需要旋转的view增加动画
-        
-        
+    }
+    
+    
+    // MARK:停止动画
+    open func stopAnimation(){
+        for subView in subviews{
+            subView.layer.removeAllAnimations()
+            subView.removeFromSuperview()
+        }
     }
     
     
